f51f6b78c83d8fc20921eba0c6e0617fe4488ec5
Nitin Gouda <nitinagouda@acpce.ac.in>
2022-01-10 16:04:00 +0530

    first commit

---
+++ Chapters/01-basic.go
@@ -0,0 +1,8 @@
+package main
+
+//we have to import package to use print statements which is inside  "fmt"
+import "fmt"
+
+func main1() {
+	fmt.Print("Hello World")
+}
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const conferenceTickets int = 50
+
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+//waits for launched go routines to wait
+var wg = sync.WaitGroup{}
+
+func main() {
+
+	greetUsers()
+
+	// for {
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+
+	if isValidName && isValidEmail && isValidTicketNumber {
+
+		bookTicket(userTickets, firstName, lastName, email)
+		//
+		wg.Add(1)
+		// we used go keyword to make our keyword concurrent
+		go sendTicket(userTickets, firstName, lastName, email)
+
+		firstNames := getFirstNames()
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			// break
+		}
+	} else {
+		if !isValidName {
+			fmt.Println("first name or last name you entered is too short")
+		}
+		if !isValidEmail {
+			fmt.Println("email address you entered doesn't contain @ sign")
+		}
+		if !isValidTicketNumber {
+			fmt.Println("number of tickets you entered is invalid")
+		}
+	}
+	//}
+	wg.Wait()
+}
